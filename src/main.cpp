9d681f7e617ac0104a198c0e14c86a57b126b0d3
Mohamed Aziz Lahdheri <118969900+MedAziz218@users.noreply.github.com>
2024-01-23 20:19:50 +0100

    added setMotor function

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,60 @@
-#include "Arduino.h" 
+#include "Arduino.h"
 
-// Define the pins for the right encoder
-const int PIN_encR_A = 35;
-const int PIN_encR_B = 19;  // Not used in this code, can be used for rotation direction sensing
+#define PWM_Res 8
+#define PWM_Freq 1200
 
-// Define the pins for the left encoder
-const int PIN_encL_A = 39;
-const int PIN_encL_B = 20;  // Not used in this code, can be used for rotation direction sensing
+#define PWM_FWD_MOTOR_L 0
+#define PWM_BWD_MOTOR_L 1
+#define PWM_FWD_MOTOR_R 2
+#define PWM_BWD_MOTOR_R 3
+
+#define PIN_FWD_MOTOR_R 26
+#define PIN_BWD_MOTOR_R 25
+#define PIN_encR_A 35
+
+#define PIN_FWD_MOTOR_L 32
+#define PIN_BWD_MOTOR_L 33
+#define PIN_encL_A 39
+void setMotor(int LL, int RR);
 
 // Variables to store encoder ticks
 volatile unsigned int encR_ticks = 0;
 volatile unsigned int encL_ticks = 0;
 
 // Function to increment right encoder tick count (Do not use directly)
-void read_rightEncoder() { 
-    encR_ticks++; 
+void read_rightEncoder()
+{
+    encR_ticks++;
 }
 
 // Function to increment left encoder tick count (Do not use directly)
-void read_leftEncoder() { 
-    encL_ticks++; 
+void read_leftEncoder()
+{
+    encL_ticks++;
 }
 
 // Function to reset right encoder tick count
-void reset_encR() { 
-    encR_ticks = 0; 
+void reset_encR()
+{
+    encR_ticks = 0;
 }
 
 // Function to reset left encoder tick count
-void reset_encL() { 
-    encL_ticks = 0; 
+void reset_encL()
+{
+    encL_ticks = 0;
 }
 
 // Function to get the current right encoder tick count (equivalent to millis())
-unsigned int get_encR() { 
-    return encR_ticks; 
+unsigned int get_encR()
+{
+    return encR_ticks;
 }
 
 // Function to get the current left encoder tick count (equivalent to millis())
-unsigned int get_encL() { 
-    return encL_ticks; 
+unsigned int get_encL()
+{
+    return encL_ticks;
 }
 
 // Setup function to configure pins and attach interrupts
@@ -48,28 +63,66 @@ void setupEncoders()
     // Configure right encoder pins
     pinMode(PIN_encR_A, INPUT_PULLUP);
     // pinMode(PIN_encR_B, INPUT_PULLUP);
-    
+
     // Configure left encoder pins
     pinMode(PIN_encL_A, INPUT_PULLUP);
     // pinMode(PIN_encL_B, INPUT_PULLUP);
-    
+
     // Attach interrupts for both encoders on rising edges
     attachInterrupt(digitalPinToInterrupt(PIN_encR_A), read_rightEncoder, RISING);
     attachInterrupt(digitalPinToInterrupt(PIN_encL_A), read_leftEncoder, RISING);
 }
 
+void setupLEDC()
+{
+ 
+    // pinMode(hbridgeA,OUTPUT); pinMode(hbridgeB,OUTPUT);
+    // pinMode(encoderPinA,INPUT);pinMode(encoderPinB,INPUT);
+    pinMode(PIN_FWD_MOTOR_L, OUTPUT);
+    ledcSetup(PWM_FWD_MOTOR_L, PWM_Freq, PWM_Res);
+    ledcAttachPin(PIN_FWD_MOTOR_L, PWM_FWD_MOTOR_L);
+
+    pinMode(PIN_BWD_MOTOR_L, OUTPUT);
+    ledcSetup(PWM_BWD_MOTOR_L, PWM_Freq, PWM_Res);
+    ledcAttachPin(PIN_BWD_MOTOR_L, PWM_BWD_MOTOR_L);
+
+    pinMode(PIN_FWD_MOTOR_R, OUTPUT);
+    ledcSetup(PWM_FWD_MOTOR_R, PWM_Freq, PWM_Res);
+    ledcAttachPin(PIN_FWD_MOTOR_R, PWM_FWD_MOTOR_R);
+
+    pinMode(PIN_BWD_MOTOR_R, OUTPUT);
+    ledcSetup(PWM_BWD_MOTOR_R, PWM_Freq, PWM_Res);
+    ledcAttachPin(PIN_BWD_MOTOR_R, PWM_BWD_MOTOR_R);
+
+    
+    setMotor(0, 0);
+
+}
 // Setup function to initialize encoders
-void setup(){
+void setup()
+{
     Serial.begin(115200);
     setupEncoders();
+    setupLEDC();
 }
 
-// Loop function 
+// Loop function (empty in this case)
 unsigned int lastEncL = 0;
 unsigned int lastEncR = 0;
-void loop(){
+int i = 0;
+void loop()
+{
     unsigned int currentEncL = get_encL();
     unsigned int currentEncR = get_encR();
+
+    // setMotor(120, 0);
+    // delay(2000);
+    // setMotor(120, 120);
+    // delay(2000);
+    // setMotor(0, 120);
+    // delay(2000);
+    // setMotor(0, 0);
+    // delay(2000);
 
     if (currentEncL>226){
         Serial.println("left wheel made 1 round");
@@ -80,6 +133,32 @@ void loop(){
         reset_encR();
     }
     Serial.print("Left Encoder: "+String(currentEncL)+" Right Encoder: "+ String(currentEncR)+"\n");
-    
+}
 
+void setMotor(int LL, int RR)
+{
+    LL = constrain(LL, -255, 255);
+    RR = constrain(RR, -255, 255);
+
+    if (RR > 0)
+    {
+        ledcWrite(PWM_FWD_MOTOR_R, RR);
+        ledcWrite(PWM_BWD_MOTOR_R, 0);
+    }
+    else
+    {
+        ledcWrite(PWM_FWD_MOTOR_R, 0);
+        ledcWrite(PWM_BWD_MOTOR_R, -RR);
+    }
+
+    if (LL > 0)
+    {
+        ledcWrite(PWM_FWD_MOTOR_L, LL);
+        ledcWrite(PWM_BWD_MOTOR_L, 0);
+    }
+    else
+    {
+        ledcWrite(PWM_FWD_MOTOR_L, 0);
+        ledcWrite(PWM_BWD_MOTOR_L, -LL);
+    }
 }
